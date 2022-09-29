ee8c59e402dde4395491ba6f4fd06b32a29acb94
davidggil30 <113586402+davidggil30@users.noreply.github.com>
2022-09-29 09:28:03 +0200

    Add files via upload

---
+++ ejercicio_debug/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AplicacionPruebaDebug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        {
+            int a = 10;
+            int b = a / 2;
+            int c = b + 7;
+            int d = c * 3;
+            double e = Math.Sin(c);
+            int f = c + d;
+        }
+    }
+}
